bda0e1e7a8b19c16cd3fecf11da2a4a669e033a0
luzart <anhmd@blackgemsglobal.com>
2024-07-24 20:46:48 +0700

    commit bo static

--- Assets/_Game/Script/UI/UISelectLevel.cs
+++ Assets/_Game/Script/UI/UISelectLevel.cs
@@ -64,7 +64,6 @@ public class UISelectLevel : UIBase
         }
         btnSelectCache = btnSelect;
         btnSelectCache.Select(true);
-        int level = DataManager.Instance.CurrentLevel;
 
         SpawnLevel(btnSelect.index);
     }
